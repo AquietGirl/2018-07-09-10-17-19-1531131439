50d7ee2f49b0db700a149377bc959b9860dd0d70
ALISA Y S HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGAL5@OOCL.COM>
2020-07-22 19:39:52 +0800

    [Alisa] feature:finish practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,25 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int classNumber;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int classNumber) {
+        super(name, age);
+        this.classNumber = classNumber;
+    }
+
+    public int getKlass() {
+        return this.classNumber;
+    }
+
+    public String introduce() {
+        if (this.classNumber != 0) {
+            return super.introduce() + " I am a Teacher. I teach Class " + this.classNumber + ".";
+        }
+        return super.introduce() + " I am a Teacher. I teach No Class.";
+    }
 }
