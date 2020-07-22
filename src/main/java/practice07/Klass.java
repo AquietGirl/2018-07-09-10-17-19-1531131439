552970dc8924180787ebadac6452230a77c86449
ALISA Y S HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGAL5@OOCL.COM>
2020-07-22 19:55:49 +0800

    [Alisa] feature:finish practice07

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,18 @@
 package practice07;
 
 public class Klass {
+    private int classNumber;
+
+    public Klass(int classNumber) {
+        this.classNumber = classNumber;
+    }
+
+    public int getNumber() {
+        return classNumber;
+    }
+
+    public String getDisplayName() {
+        return "Class " + classNumber;
+    }
+
 }
