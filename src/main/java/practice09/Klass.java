4a8e33b970fd93d5c6a59ffa55297cec8cdafdb0
ALISA Y S HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGAL5@OOCL.COM>
2020-07-22 21:17:43 +0800

    [Alisa] feature:finish practice09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,39 @@
 package practice09;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int classNumber;
+    private Student leader;
+    private List<Student> studentList = new ArrayList<>();
+
+    public Klass (int classNumber) {
+        this.classNumber = classNumber;
+    }
+
+    public int getNumber() {
+        return this.classNumber;
+    }
+
+    public String getDisplayName() {
+        return "Class " + this.classNumber;
+    }
+
+    public void assignLeader(Student student) {
+        if (this.studentList != null && this.studentList.contains(student)) {
+            this.leader = student;
+        }
+        System.out.print("It is not one of us.\n");
+    }
+
+    public Student getLeader() {
+        return this.leader;
+    }
+
+    public void appendMember(Student student) {
+        if (student != null){
+            this.studentList.add(student);
+        }
+    }
 }
