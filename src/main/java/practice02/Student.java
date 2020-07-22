713656eef6c68bf1113d5283f553b40974143e56
ALISA Y S HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGAL5@OOCL.COM>
2020-07-22 18:23:39 +0800

    [Alisa] feature:finish practice02

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,19 @@
 package practice02;
 
-public class Student {
+public class Student extends Person {
+
+    private int classNumber;
+
+    public Student(String name, int age, int classNumber) {
+        super(name, age);
+        this.classNumber = classNumber;
+    }
+
+    public int getKlass () {
+        return this.classNumber;
+    }
+
+    public String introduce() {
+        return "I am a Student. I am at Class " + this.classNumber + ".";
+    }
 }
